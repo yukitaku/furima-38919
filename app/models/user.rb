fa001b1e35a1e4fbd4424342fdb304946f552a86
tc-user <tc-user@A-499.local>
2023-05-18 09:56:05 +0900

    ユーザー管理

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :name, presence: true
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday, presence: true      
+
+  has_many :items
+  has_many :histories
 end
