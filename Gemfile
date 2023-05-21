84c3dc4d47650089a00c9dca73e076541ac404c2
tc-user <tc-user@A-499.local>
2023-05-21 22:09:22 +0900

    新規登録

--- Gemfile
+++ Gemfile
@@ -62,3 +62,8 @@ group :production do
 end
 
 gem 'devise'
+group :development, :test do
+  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'rspec-rails', '~> 4.0.0'
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力して下さい。'}, presence: true
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力して下さい。'}, presence: true
+  validates :last_name_kana,format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'は全角カタカナで入力して下さい。'}, presence: true
+  validates :first_name_kana,format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'は全角カタカナで入力して下さい。'}, presence: true
   validates :birthday, presence: true      
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password,format: { with:VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}, presence: true
 
   has_many :items
   has_many :histories
