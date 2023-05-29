b69c4f6ea85b89236d557b068aaa0f73d48e8a61
tc-user <tc-user@A-499.local>
2023-05-30 07:25:52 +0900

    商品出品機能

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+
+    item_name {Faker::Lorem.sentence}
+    catch_copy {Faker::Lorem.sentence}
+    category_id{2}
+    condition_id{2}
+    period_id{2}
+    place_id{2}
+    okuru_id{2}
+    price{350}
+
+    association :user   
+    after(:build) do |tweet|
+      tweet.image.attach(io: File.open('spec/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
