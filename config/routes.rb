efe6b64c06ee45991fad697153246e2e6ac4707a
tc-user <tc-user@A-499.local>
2023-06-01 06:38:06 +0900

    商品詳細機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
+  resources :items do
   end
   resources :users, only: :show
 
