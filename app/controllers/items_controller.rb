6d39637d89e3305822126d2b7c171ac119a0c91d
tc-user <tc-user@A-499.local>
2023-06-02 21:31:10 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
   before_action :authenticate_user!, except: [:index, :show]
-   before_action :set_item, only: [:show, :edit, :update,]
+   before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index
 
     @items = Item.includes(:user).order('created_at DESC')
@@ -39,13 +39,14 @@ class ItemsController < ApplicationController
      end
    end
 
-  # def destroy
-  #   if @item.destroy
-  #     redirect_to root_path
-  #   else
-  #     redirect_to root_path
-  #   end
-  # end
+  def destroy
+    if @item.user_id == current_user.id
+       @item.destroy
+       redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
 
 
   private
