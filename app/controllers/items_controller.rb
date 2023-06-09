a1e9a9c6d3894cde3fa8667d14bf1ab6006ff4a6
tc-user <tc-user@A-499.local>
2023-06-09 18:31:25 +0900

    ４

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -42,6 +42,7 @@ class ItemsController < ApplicationController
   def destroy
     if @item.user_id == current_user.id
        @item.destroy
+       redirect_to root_path
     else
       redirect_to root_path
     end
